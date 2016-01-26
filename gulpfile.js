0fe33ba01ccbb9523ab9621e51883536ccbc6ff1
markushausammann <markus.hausammann@cloud-solutions.net>
2016-01-26 17:37:17 +0200

    first draft solution

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var gzip = require('gulp-gzip');
+var minify = require('gulp-cssnano');
+var less = require('gulp-less');
+var watchLess = require('gulp-watch-less');
+
+var publicPath = 'public/';
+
+gulp.task('default', ['prod']);
+
+gulp.task('prod', []);
+gulp.task('dev', []);
+
+gulp.task('compile-less', function() {
+    return gulp.src(publicPath + 'less/main.less')
+        .pipe(less())
+        .pipe(gulp.dest(publicPath + 'css'));
+});
+
+gulp.task('watch-less', function() {
+    return gulp.src(publicPath + 'less/main.less')
+        .pipe(watchLess(publicPath + 'less/main.less'))
+        .pipe(less())
+        .pipe(gulp.dest(publicPath + 'css'));
+});
